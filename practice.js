2e691a908f82ff7a482d6d821a391a885c62218b
Parker Jones <parker.harold.jones@gmail.com>
2021-03-06 20:35:45 -0700

    First three chapters

--- practice.js
+++ practice.js
@@ -1,11 +1,16 @@
-alert ( "I like making TikToks" ); //chapter one
+alert ( "Parker likes making TikToks" ); //chapter one
 
-var name = "Marco";// chapter two
+var name = "Parker";// chapter two
 var nationality; 
-nationality = "Germany";
+nationality = "United States";
 alert (name);
-alert (nationality); //chapter two
+alert ("His nationality is the " + nationality); //chapter two
 
+var weight = 185; //chapter three 
+var projectedweight = weight + 10;
+var newweight = projectedweight; 
+alert ("His current weight is " + weight);
+alert  ("His projected weight at 27 is " + newweight); //chapter three
 
 
 
