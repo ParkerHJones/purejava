33bd12a97d5c2fe57225e6cae0d06136ec80764f
Parker Jones <parker.harold.jones@gmail.com>
2021-03-06 20:15:18 -0700

    initial push

---
+++ practice.js
@@ -0,0 +1,11 @@
+alert ( "I like making TikToks" ); //chapter one
+
+var name = "Marco";// chapter two
+var nationality; 
+nationality = "Germany";
+alert (name);
+alert (nationality); //chapter two
+
+
+
+
