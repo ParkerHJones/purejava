20731c3b0fe02dd7a3ddd3e458256758b4432a14
Parker Jones <parker.harold.jones@gmail.com>
2021-03-07 07:45:47 -0700

    chapters 4-6 done

--- practice.js
+++ practice.js
@@ -15,3 +15,21 @@ alert  ("His projected weight at 27 is " + newWeight); //chapter three
 var hairColor; // using camelcase from chapter 4
 hairColor = "Bald"; 
 alert (hairColor); // camelcase from chapter 4 
+
+alert (2+5); //chapter 5 math expressions 
+var seven = 7;
+alert (seven); 
+var eight = 4+4;
+alert (eight); 
+var eighteen = 6 * 3; 
+alert (eighteen); 
+var three = 4-1;
+alert (three); 
+var ten = 100/10;
+alert (ten); 
+var answerOne = 15 % 4;
+alert (answerOne); //chapter 5 math expressions 
+
+var num = 1; // chapter 6 
+var newNum = ++num; // this adds one to var num
+alert (newNum);  // chapter 6 unfamiliar expression 
