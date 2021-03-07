4f26475ae8e35bd7c7f60a2b5ee44fc46de86a30
Parker Jones <parker.harold.jones@gmail.com>
2021-03-07 07:21:20 -0700

    chapter 4

--- practice.js
+++ practice.js
@@ -8,9 +8,10 @@ alert ("His nationality is the " + nationality); //chapter two
 
 var weight = 185; //chapter three 
 var projectedweight = weight + 10;
-var newweight = projectedweight; 
+var newWeight = projectedweight; // using camel case from chapter 4 
 alert ("His current weight is " + weight);
-alert  ("His projected weight at 27 is " + newweight); //chapter three
+alert  ("His projected weight at 27 is " + newWeight); //chapter three
 
-
-
+var hairColor; // using camelcase from chapter 4
+hairColor = "Bald"; 
+alert (hairColor); // camelcase from chapter 4 
