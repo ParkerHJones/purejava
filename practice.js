d3d86a11118a2f422b31b3432f8ea177b736de29
Parker Jones <parker.harold.jones@gmail.com>
2021-03-09 09:35:13 -0700

    completed chapters 10-12

--- practice.js
+++ practice.js
@@ -1,67 +1,93 @@
-alert ( "Parker likes making TikToks" ); //chapter one
+// alert ( "Parker likes making TikToks" ); //chapter one
 
-var name = "Parker";// chapter two
-var nationality; 
-nationality = "United States";
-alert (name);
-alert ("His nationality is the " + nationality); //chapter two
+// var name = "Parker";// chapter two
+// var nationality; 
+// nationality = "United States";
+// alert (name);
+// alert ("His nationality is the " + nationality); //chapter two
 
-var weight = 185; //chapter three 
-var projectedweight = weight + 10;
-var newWeight = projectedweight; // using camel case from chapter 4 
-alert ("His current weight is " + weight);
-alert  ("His projected weight at 27 is " + newWeight); //chapter three
+// var weight = 185; //chapter three 
+// var projectedweight = weight + 10;
+// var newWeight = projectedweight; // using camel case from chapter 4 
+// alert ("His current weight is " + weight);
+// alert  ("His projected weight at 27 is " + newWeight); //chapter three
 
-var hairColor; // using camelcase from chapter 4
-hairColor = "Bald"; 
-alert (hairColor); // camelcase from chapter 4 
+// var hairColor; // using camelcase from chapter 4
+// hairColor = "Bald"; 
+// alert (hairColor); // camelcase from chapter 4 
 
-alert (2+5); //chapter 5 math expressions 
-var seven = 7;
-alert (seven); 
-var eight = 4+4;
-alert (eight); 
-var eighteen = 6 * 3; 
-alert (eighteen); 
-var three = 4-1;
-alert (three); 
-var ten = 100/10;
-alert (ten); 
-var answerOne = 15 % 4;
-alert (answerOne); //chapter 5 math expressions 
+// alert (2+5); //chapter 5 math expressions 
+// var seven = 7;
+// alert (seven); 
+// var eight = 4+4;
+// alert (eight); 
+// var eighteen = 6 * 3; 
+// alert (eighteen); 
+// var three = 4-1;
+// alert (three); 
+// var ten = 100/10;
+// alert (ten); 
+// var answerOne = 15 % 4;
+// alert (answerOne); //chapter 5 math expressions 
 
-var num = 1; // chapter 6 
-var newNum = ++num; // this adds one to var num displays 2
-alert (newNum);  // chapter 6 unfamiliar expression
+// var num = 1; // chapter 6 
+// var newNum = ++num; // this adds one to var num displays 2
+// alert (newNum);  // chapter 6 unfamiliar expression
 
-// chapter 7 
-var total = 3 + (8 * 8); // = 67
-var totalTwo = (4 + 4) * 4; // = 32
-var totalThree = ((1 * 3) * 4) + 5 // = 17
-var totalFour = (5 * 5) - (6 / 2) // = 22
-alert (total);
-alert (totalTwo);
-alert (totalThree);
-alert (totalFour); // chapter 7 
+// // chapter 7 
+// var total = 3 + (8 * 8); // = 67
+// var totalTwo = (4 + 4) * 4; // = 32
+// var totalThree = ((1 * 3) * 4) + 5 // = 17
+// var totalFour = (5 * 5) - (6 / 2) // = 22
+// alert (total);
+// alert (totalTwo);
+// alert (totalThree);
+// alert (totalFour); // chapter 7 
 
-// chapter 8
-var userName = "iPOD"; // needs to be above alert or will return "undefined for userName"
-alert ("Your welcome, " + userName + "!");
-var header = "Two"; 
-var customAlert = userName + header; 
-alert(customAlert); //concatenation 
+// // chapter 8
+// var userName = "iPOD"; // needs to be above alert or will return "undefined for userName"
+// alert ("Your welcome, " + userName + "!");
+// var header = "Two"; 
+// var customAlert = userName + header; 
+// alert(customAlert); //concatenation 
 
-alert("1" + "1"); // displays 11 
-// chapter 8 
+// alert("1" + "1"); // displays 11 
+// // chapter 8 
 
-// chapter 9 prompt code 
-var question = "Your favorite color?";
-var defAnswer = "Blue";
-var output = prompt(question, defAnswer);  //displays prompt with question and answer Blue already there
+// // chapter 9 prompt code 
+// var question = "Your favorite color?";
+// var defAnswer = "Blue";
+// var output = prompt(question, defAnswer);  //displays prompt with question and answer Blue already there
 
-var numberofDogs = prompt("How many dogs do you have?");
-var toomanyDogs = numberofDogs + 1; 
-alert (toomanyDogs); // displays whatever number input with one added to the end of it
-// chapter 9 
+// var numberofDogs = prompt("How many dogs do you have?");
+// var toomanyDogs = numberofDogs + 1; 
+// alert (toomanyDogs); // displays whatever number input with one added to the end of it
+// // chapter 9 
 
+// chapter 10 IF STATEMENTS
+var goat = prompt("Who is GOAT in reggaeton?");
+if (goat === "bad bunny") {
+    alert ("YA TU SABES"); 
+}
+// simple if statement 
+// chapter 10
+
+// chapter 11 comparison operators 
+// === is a comparison operator
+// there are various other ones
+
+// chapter 12 if else..else if
+var x = prompt("Where is Bad Bunny from?");
+if (x === "puerto rico") {
+    alert ("BUEN HECHO!");
+}
+// if (x !== "puerto rico"){ 
+    else if (x === "Puerto Rico") {
+    alert("LO ESCRIBISTE MAL");
+}
+else { 
+    alert("TE EQUIVOCASTE")
+}
+
+// chapter 13 
 
